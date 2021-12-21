863d193607cbe15f3bb2c36019d5567274c3c6cd
Simon Ledoux <simon@simon511000.fr>
2021-12-21 11:07:01 +0100

    Commit initial : structure de base du plugin

---
+++ src/main/java/fr/endit/stargate/Stargate.java
@@ -0,0 +1,10 @@
+package fr.endit.stargate;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Stargate extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello");
+    }
+}
